2eeadd2d8e6c8a2fd94696a8ec4c4dd5e5b20132
Marek Materzok <tilk@tilk.eu>
2021-08-23 17:20:08 +0200

    Start unit testing.

---
+++ tests/unittests.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+import Prelude
+import qualified Clash.Prelude as CP
+import Test.Tasty
+import qualified Hedgehog as H
+import qualified Test.Tasty.Hedgehog as TH
+import qualified Test.Tasty.HUnit as TU
+
+import FSM
+
+main :: IO ()
+main = defaultMain $ testGroup "."
+  [ 
+    TU.testCase "upcount" $ CP.simulateN @CP.System 100 myFsm (repeat ()) TU.@?= [(0 :: Integer)..99]
+--    TH.testProperty "upcount" $ H.property $ 1 H.=== 1
+  ]
+
+myFsm = CP.mealy fsmFunc_fsm fsmInitState_fsm
+
+[fsm|inputs ()
+fun f i
+    begin
+        emit i
+        ret call f (i+1)
+    end
+ret call f 0
+|]
+
