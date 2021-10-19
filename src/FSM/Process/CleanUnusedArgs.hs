e15144c59493f659f966be56e564dee132932b3b
Marek Materzok <tilk@tilk.eu>
2021-10-19 22:18:23 +0200

    Removing unused single parameter (change to unit)

--- src/FSM/Process/CleanUnusedArgs.hs
+++ src/FSM/Process/CleanUnusedArgs.hs
@@ -9,54 +9,65 @@ import qualified Data.Set as S
 import qualified Data.Map.Strict as M
 import qualified Language.Haskell.TH as TH
 
-canCleanVStmt :: VStmt -> S.Set TH.Name
-canCleanVStmt (VExp _) = S.empty
-canCleanVStmt (VCall n e) = canCleanExp n e
+type UFMap = M.Map TH.Name (Either Bool [Bool])
 
-canCleanStmt :: Stmt -> S.Set TH.Name
-canCleanStmt SNop = S.empty
-canCleanStmt (SLet _ _ vs s) = canCleanVStmt vs `S.union` canCleanStmt s
-canCleanStmt (SAssign _ vs) = canCleanVStmt vs
-canCleanStmt (SYield _) = S.empty
-canCleanStmt (SRet vs) = canCleanVStmt vs
-canCleanStmt (SBlock ss) = S.unions $ map canCleanStmt ss
-canCleanStmt (SIf _ st sf) = canCleanStmt st `S.union` canCleanStmt sf
-canCleanStmt (SCase _ cs) = S.unions $ map (canCleanStmt . snd) cs
-canCleanStmt (SFun _ _) = error "Not in lambda-lifted form"
+canCleanVStmt :: UFMap -> VStmt -> S.Set TH.Name
+canCleanVStmt _ (VExp _) = S.empty
+canCleanVStmt m (VCall n e) = canCleanExp m n e
 
-canCleanFunMap :: FunMap -> S.Set TH.Name
-canCleanFunMap = S.unions . map f . M.toList
+canCleanStmt :: UFMap -> Stmt -> S.Set TH.Name
+canCleanStmt _ SNop = S.empty
+canCleanStmt m (SLet _ _ vs s) = canCleanVStmt m vs `S.union` canCleanStmt m s
+canCleanStmt m (SAssign _ vs) = canCleanVStmt m vs
+canCleanStmt _ (SYield _) = S.empty
+canCleanStmt m (SRet vs) = canCleanVStmt m vs
+canCleanStmt m (SBlock ss) = S.unions $ map (canCleanStmt m) ss
+canCleanStmt m (SIf _ st sf) = canCleanStmt m st `S.union` canCleanStmt m sf
+canCleanStmt m (SCase _ cs) = S.unions $ map (canCleanStmt m . snd) cs
+canCleanStmt _ (SFun _ _) = error "Not in lambda-lifted form"
+
+canCleanFunMap :: UFMap -> FunMap -> S.Set TH.Name
+canCleanFunMap m = S.unions . map f . M.toList
     where 
-    f (n, (p, s)) = canCleanPat n p `S.union` canCleanStmt s
+    f (n, (p, s)) = canCleanPat m n p `S.union` canCleanStmt m s
 
-canCleanPat :: TH.Name -> TH.Pat -> S.Set TH.Name
-canCleanPat _ (TH.TupP _) = S.empty
-canCleanPat n _ = S.singleton n
+canCleanPat :: UFMap -> TH.Name -> TH.Pat -> S.Set TH.Name
+canCleanPat m n p | Just (Right bs) <- r, or bs, TH.TupP ps <- p, length bs == length ps = S.empty
+                  | Just (Left True) <- r = S.empty
+                  | otherwise = S.singleton n
+    where r = M.lookup n m
 
-canCleanExp :: TH.Name -> TH.Exp -> S.Set TH.Name
-canCleanExp _ (TH.TupE mes) | all isJust mes = S.empty
-canCleanExp n _ = S.singleton n
+canCleanExp :: UFMap -> TH.Name -> TH.Exp -> S.Set TH.Name
+canCleanExp m n e | Just (Right bs) <- r, or bs, TH.TupE mes <- e, all isJust mes, length bs == length mes = S.empty
+                  | Just (Left True) <- r = S.empty
+                  | otherwise = S.singleton n
+    where r = M.lookup n m
 
-unusedFunMap :: FunMap -> M.Map TH.Name [Bool]
+unusedFunMap :: FunMap -> UFMap
 unusedFunMap = M.map f where
-    f (TH.TupP ps, s) = map g ps where
+    f (p, s) | TH.TupP ps <- p = Right $ map g ps
+             | otherwise       = Left $ g p
+        where
         fvs = freeVars s
-        g p = S.null $ boundVars p `S.intersection` fvs
-    f (_, _) = []
+        g p' = S.null $ boundVars p' `S.intersection` fvs
 
-doCleanPat :: M.Map TH.Name [Bool] -> TH.Name -> TH.Pat -> TH.Pat
-doCleanPat m n p | Just bs <- M.lookup n m, TH.TupP ps <- p = tupP $ map snd $ filter (not . fst) $ zip bs ps
+doCleanPat :: UFMap -> TH.Name -> TH.Pat -> TH.Pat
+doCleanPat m n p | Just (Right bs) <- r, TH.TupP ps <- p = tupP $ map snd $ filter (not . fst) $ zip bs ps
+                 | Just (Left b) <- r = if b then tupP [] else p
                  | otherwise = p
+    where r = M.lookup n m
 
-doCleanExp :: M.Map TH.Name [Bool] -> TH.Name -> TH.Exp -> TH.Exp
-doCleanExp m n e | Just bs <- M.lookup n m, TH.TupE mes <- e = tupE $ map snd $ filter (not . fst) $ zip bs $ map fromJust mes
+doCleanExp :: UFMap -> TH.Name -> TH.Exp -> TH.Exp
+doCleanExp m n e | Just (Right bs) <- r, TH.TupE mes <- e = tupE $ map snd $ filter (not . fst) $ zip bs $ map fromJust mes
+                 | Just (Left b) <- r = if b then tupE [] else e
                  | otherwise = e
+    where r = M.lookup n m
 
-doCleanVStmt :: M.Map TH.Name [Bool] -> VStmt -> VStmt
+doCleanVStmt :: UFMap -> VStmt -> VStmt
 doCleanVStmt _ vs@(VExp _) = vs
 doCleanVStmt m (VCall n e) = VCall n (doCleanExp m n e)
 
-doCleanStmt :: M.Map TH.Name [Bool] -> Stmt -> Stmt
+doCleanStmt :: UFMap -> Stmt -> Stmt
 doCleanStmt _ s@(SNop) = s
 doCleanStmt m   (SLet n t vs s) = SLet n t (doCleanVStmt m vs) (doCleanStmt m s)
 doCleanStmt m   (SAssign n vs) = SAssign n (doCleanVStmt m vs)
@@ -67,7 +78,7 @@ doCleanStmt m   (SIf e st sf) = SIf e (doCleanStmt m st) (doCleanStmt m sf)
 doCleanStmt m   (SCase e cs) = SCase e $ map (id *** doCleanStmt m) cs
 doCleanStmt _   (SFun _ _) = error "Not in lambda-lifted form"
 
-doCleanFunMap :: M.Map TH.Name [Bool] -> FunMap -> FunMap
+doCleanFunMap :: UFMap -> FunMap -> FunMap
 doCleanFunMap m = M.mapWithKey f where
     f n (p, s) = (doCleanPat m n p, doCleanStmt m s)
 
@@ -77,7 +88,8 @@ cleanUnusedArgs prog = prog {
     nProgInitParam = doCleanExp unuseds (nProgInit prog) (nProgInitParam prog)
 }
     where
-    nonCleanSet = canCleanFunMap (nProgFuns prog) `S.union` canCleanExp (nProgInit prog) (nProgInitParam prog)
-    unuseds = unusedFunMap (nProgFuns prog) `M.difference` M.fromSet (const []) nonCleanSet
+    nonCleanSet = canCleanFunMap ufm (nProgFuns prog) `S.union` canCleanExp ufm (nProgInit prog) (nProgInitParam prog)
+    unuseds = ufm `M.difference` M.fromSet (const []) nonCleanSet
+    ufm = unusedFunMap (nProgFuns prog)
 
 
