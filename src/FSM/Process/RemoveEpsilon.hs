30c105beb8c32b5610b5c69587291496394325f4
Marek Materzok <tilk@tilk.eu>
2022-06-24 11:04:06 +0200

    Detect and report cycles without yield

--- src/FSM/Process/RemoveEpsilon.hs
+++ src/FSM/Process/RemoveEpsilon.hs
@@ -16,7 +16,8 @@ import FSM.Util.MonadRefresh
 data REData l = REData {
     _reDataFunMap :: FunMap l,
     _reDataInputVars :: S.Set TH.Name,
-    _reDataEmitted :: Bool
+    _reDataEmitted :: Bool,
+    _reDataVisited :: S.Set TH.Name
 }
 
 $(makeLenses ''REData)
@@ -27,7 +28,7 @@ removeEpsilonStmt   (SIf e st sf) = SIf e <$> removeEpsilonStmt st <*> removeEps
 removeEpsilonStmt   (SLet t ln vs s) = SLet t ln vs <$> removeEpsilonStmt s
 removeEpsilonStmt   (SCase e cs) = SCase e <$> mapM cf cs where
     cf (p, s) = (p,) <$> removeEpsilonStmt s
-removeEpsilonStmt   (SYieldT e s) = SYieldT e <$> locally reDataEmitted (const True) (removeEpsilonStmt s)
+removeEpsilonStmt   (SYieldT e s) = SYieldT e <$> locally reDataEmitted (const True) (locally reDataVisited (const S.empty) $ removeEpsilonStmt s)
 removeEpsilonStmt s@(SRet (VCall f e)) = do
     (p, s') <- views reDataFunMap $ fromJust . M.lookup f
     em <- view reDataEmitted
@@ -35,8 +36,10 @@ removeEpsilonStmt s@(SRet (VCall f e)) = do
     b <- gets (M.member f)
     if em && (b || emittingStmt s' || not (S.null $ S.intersection ivs $ freeVars s')) then removeEpsilonFrom f >> return s
     else do
+        vis <- view reDataVisited
+        when (S.member f vis) $ error $ "cycle without yield: " ++ show vis
         (p', su) <- refreshPat p
-        SCase e <$> (return . (p',) <$> removeEpsilonStmt (rename su s'))
+        locally reDataVisited (S.insert f) $ SCase e <$> (return . (p',) <$> removeEpsilonStmt (rename su s'))
 removeEpsilonStmt s = error $ "removeEpsilonStmt statement not in tree form: " ++ show s
 
 removeEpsilonFrom :: (IsLowered l, MonadRefresh f, MonadReader (REData l) f, MonadState (M.Map TH.Name (TH.Pat, Stmt l)) f) =>
@@ -46,11 +49,11 @@ removeEpsilonFrom f = do
     b <- gets (M.member f)
     unless b $ do
         modify $ M.insert f (p, error "BUG in removeEpsilon")
-        s' <- locally reDataEmitted (const False) $ removeEpsilonStmt s
+        s' <- locally reDataEmitted (const False) $ locally reDataVisited (const S.empty) $ removeEpsilonStmt s
         modify $ M.insert f (p, s')
 
 removeEpsilon :: (IsLowered l, MonadRefresh m) => NProg l -> m (NProg l)
 removeEpsilon prog = do
-    fs' <- flip execStateT M.empty $ flip runReaderT (REData (nProgFuns prog) (boundVars $ nProgInputs prog) False) $ removeEpsilonFrom (nProgInit prog)
+    fs' <- flip execStateT M.empty $ flip runReaderT (REData (nProgFuns prog) (boundVars $ nProgInputs prog) False S.empty) $ removeEpsilonFrom (nProgInit prog)
     return $ prog { nProgFuns = fs' }
 
